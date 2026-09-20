a129db74908486e724caba147bedc23df7e07238
anpavel84-afk <anpavel84@gmail.com>
2026-09-20 09:51:32 -0300

    Build Bambini Living v5 HLS WebView test

---
+++ android-v50/app/src/main/java/ru/anpavel/bambiniliving/MainActivity.java
@@ -0,0 +1,67 @@
+package ru.anpavel.bambiniliving;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final String URL = "http://169.58.183.182:18080/apk.html";
+    private WebView web;
+
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        web = new WebView(this);
+        web.setBackgroundColor(Color.BLACK);
+        web.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
+        WebSettings s = web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        s.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        s.setCacheMode(WebSettings.LOAD_DEFAULT);
+        s.setAllowContentAccess(true);
+        s.setAllowFileAccess(false);
+
+        web.setWebChromeClient(new WebChromeClient());
+        web.setWebViewClient(new WebViewClient());
+        setContentView(web);
+        immersive();
+        web.loadUrl(URL);
+    }
+
+    private void immersive() {
+        web.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
+
+    @Override public void onWindowFocusChanged(boolean focus) {
+        super.onWindowFocusChanged(focus);
+        if (focus) immersive();
+    }
+
+    @Override public void onBackPressed() {
+        web.reload();
+    }
+
+    @Override protected void onDestroy() {
+        if (web != null) {
+            web.loadUrl("about:blank");
+            web.destroy();
+        }
+        super.onDestroy();
+    }
+}
